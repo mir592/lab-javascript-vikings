acc50ac7f4d2b246f779533c5619b0650468a721
mir592 <miriam5592@gmail.com>
2017-08-20 12:56:51 +0200

    finish (more or less)

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -74,17 +74,41 @@ War.prototype.addSaxon = function(Saxon){
   this.saxonArmy.push(Saxon);
 };
 
+};
 War.prototype.vikingAttack = function(){
-  var vattack = this.vikingArmy[0].attack();
-  this.saxonArmy[0].receiveDamage(vattack);
-  this.saxonArmy.shift();
-  return;
-};
+     var saxonRand = parseInt(Math.random()*((this.saxonArmy).length)); //Cogemos a un saxon aleatorio
+     var vikingRand = parseInt(Math.random()*((this.vikingArmy).length)); //cogemos a un viking aleatorio
+     this.saxonArmy[saxonRand].health -= this.vikingArmy[vikingRand].strength;
+     var result = this.saxonArmy[saxonRand].name + " has received " + this.vikingArmy[vikingRand].strength + " points of damage"; //(esta frase viene en VikingSpcec.HTML)
+     if(this.saxonArmy[saxonRand].health<=0){
+       this.saxonArmy.splice(saxonRand,1); // splice borra del array saxonArmy el elemento de índice [saxonRand] y el número de elementos (1) desde saxonRand
+       result = "A Saxon has died in combat";
+       return result;
+     }
+ };
 
-War.prototype.saxonAttack = function(){
-  var sattack = this.saxonArmy[0].attack();
-  this.vikingArmy[0].receiveDamage(sattack);
-  for (var i = 0; i < 12; i += 1) {
-  this.vikingArmy.shift();}
-  return;
-};
+
+ War.prototype.saxonAttack = function(){
+      var saxonRand = parseInt(Math.random()*((this.saxonArmy).length)); //Cogemos a un saxon aleatorio
+      var vikingRand = parseInt(Math.random()*((this.vikingArmy).length)); //cogemos a un viking aleatorio
+      this.vikingArmy[vikingRand].health -= this.saxonArmy[saxonRand].strength;
+      var result = this.vikingArmy[vikingRand].name + " has received " + this.saxonArmy[saxonRand].strength + " points of damage"; //(esta frase viene en VikingSpcec.HTML)
+      if(this.vikingArmy[vikingRand].health<=0){
+        this.vikingArmy.splice(vikingRand,1); // splice borra del array vikingArmy el elemento de índice [vikingRand] y el número de elementos (1) desde vikingRand
+        result = viking.name + " has received " + saxon.strength + " points of damage";
+        return result;
+      }
+  };
+
+
+ War.prototype.showStatus = function(){
+     var result;
+     if (this.saxonArmy.length === 0){
+       result = "Vikings have won the war of the century!";
+     }else if (this.vikingArmy.length === 0) {
+       result= "Saxons have fought for their lives and survive another day...";
+     }else{
+       result= "Vikings and Saxons are still in the thick of battle.";
+     }
+     return result;
+ };
